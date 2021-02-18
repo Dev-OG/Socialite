c5d217cf657b539363a3c654a75e88ad72bc3722
Andy Malpart <originegraphix@gmail.com>
2021-02-18 18:01:47 -0500

    Create tokenize system

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,11 @@
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: 3 * 24 * 60 * 60 * 1000
+    })
+};
 
 module.exports.signUp = async (req, res) => {
     const {pseudo, email, password} = req.body
@@ -10,4 +17,20 @@ module.exports.signUp = async (req, res) => {
     catch(err) {
         res.status(200).send({err})
     }
+};
+
+module.exports.logIn = async (req, res) => {
+    const { email, password} = req.body;
+
+    try{
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id)
+    } catch (err){
+
+    }
+};
+
+module.exports.logOut = async (req, res) => {
+
 }
+
