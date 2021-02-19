3c61925277c23982844d0bb1e0973c3bc97cc69e
Andy Malpart <originegraphix@gmail.com>
2021-02-19 10:09:22 -0500

    add decrypt to password at login

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect(
-        "mongodb+srv://" + process.env.DB_USER_PASS + "@socialite.2lkg8.mongodb.net/socialite",
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreativeIndex: true,
-            useFindAndModify: false,
-        }
-    )
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Failed to MongoDB connect", err))
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER_PASS +
+      "@socialite.2lkg8.mongodb.net/socialite",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreativeIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to MongoDB connect", err));
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,36 +1,35 @@
-const UserModel = require('../models/user.model');
-const jwt = require('jsonwebtoken');
+const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
+const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.TOKEN_SECRET, {
-        expiresIn: 3 * 24 * 60 * 60 * 1000
-    })
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
 };
 
 module.exports.signUp = async (req, res) => {
-    const {pseudo, email, password} = req.body
+  const { pseudo, email, password } = req.body;
 
-    try {
-        const user = await UserModel.create({pseudo, email, password});
-        res.status(201).json({user: user._id});
-    }
-    catch(err) {
-        res.status(200).send({err})
-    }
+  try {
+    const user = await UserModel.create({ pseudo, email, password });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).send({ err });
+  }
 };
 
 module.exports.logIn = async (req, res) => {
-    const { email, password} = req.body;
+  const { email, password } = req.body;
 
-    try{
-        const user = await UserModel.login(email, password);
-        const token = createToken(user._id)
-    } catch (err){
-
-    }
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user.id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).send({ err });
+  }
 };
 
-module.exports.logOut = async (req, res) => {
-
-}
-
+module.exports.logOut = async (req, res) => {};
