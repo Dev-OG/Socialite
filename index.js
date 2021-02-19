a4ad26084b105e3d12fa8fe34f4c23f57a0c371f
Andy Malpart <originegraphix@gmail.com>
2021-02-19 17:48:20 -0500

    CRUD Post created

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const {checkUser, requireAuth} = require('./middleware/auth.middleware'); 
+const postRoutes = require("./routes/post.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,13 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //jwt
-app.get('*', checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
 });
 
 //routes
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 //Server
 app.listen(process.env.PORT, () => {
